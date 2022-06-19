eb43096a002c63b2cca54d93bfaa9a79e2ae1b42
Sirah <sirahe@Sirahs-MacBook-Pro.local>
2022-06-20 00:33:16 +0800

    initial commit

---
+++ Server/Config/db.ts
@@ -0,0 +1,4 @@
+export const LocalURI = "mongodb://127.0.0.1/media";
+export const RemoteURI = "mongodb+srv://sirah:VapWFARmyA7K0vAz@cluster0.xsh4za9.mongodb.net/?retryWrites=true&w=majority";
+export const HostName = "MongoDB Atlas";
+export const Secret = "someSecret";
